5bba15cbbef954516a9164928181e7f29300a179
h15200 <45977354+h15200@users.noreply.github.com>
2020-02-29 23:44:32 -0500

    start background debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
 import React from "react"
+import BackgroundImage from "gatsby-background-image"
 import { Link } from "gatsby"
-import Layout from "../components/layout"
-import BackgroundImage from "../components/background-image"
+import { useStaticQuery, graphql } from "gatsby"
+
 import SEO from "../components/seo"
 
-import indexStyles from '../styles/modules/index.module.scss'
+import indexStyles from "../styles/modules/index.module.scss"
 
-const IndexPage = () => (
-  <>
-    <div className={indexStyles.container}>
-      <BackgroundImage className={indexStyles.backgroundImg} />
-      <h1>Rob Moose</h1>
-    </div>
-  </>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      background: file(relativePath: { eq: "background.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 2200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <>
+      <SEO title="Home"/>
+      <BackgroundImage
+      className={indexStyles.backgroundImg}
+      fluid={data.background.childImageSharp.fluid}
+      >
+        <header>Header</header>
+        <h1>Rob Moose</h1>
+        <footer>© {new Date().getFullYear()}</footer>
+      </BackgroundImage>
+    </>
+  )
+}
 
 export default IndexPage
